349b9618310c49748b7b3b10212eeec3be8b7f53
Irlanda <i.landa2295@gmail.com>
2025-10-22 23:30:01 -0400

    proyecto 19 segunda parte

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,3 +5,5 @@ const {createUser, login}=require('../controllers/auth.js');
 // Rutas públicas con validación
 router.post('/signup', createUser);
 router.post('/signin', login);
+
+module.exports = router;
